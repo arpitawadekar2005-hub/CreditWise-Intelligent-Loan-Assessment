c6d6aac678b9e35558c46768943922fada9ddc3c
arpitawadekar2005-hub <arpitawadekar2005@gmail.com>
2026-06-20 17:30:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 import streamlit as st
-import os
+import pickle
+import numpy as np
 
-st.set_page_config(page_title="CreditWise Debug")
+model = pickle.load(open("model.pkl", "rb"))
 
-st.title("🔍 CreditWise Debug")
+st.title("CreditWise Loan Approval System")
 
-st.write("Current working directory:")
-st.write(os.getcwd())
+income = st.number_input("Applicant Income", min_value=0)
+co_income = st.number_input("Coapplicant Income", min_value=0)
+age = st.number_input("Age", min_value=18)
+credit_score = st.number_input("Credit Score", min_value=300, max_value=900)
+loan_amount = st.number_input("Loan Amount", min_value=0)
+loan_term = st.number_input("Loan Term (Months)", min_value=1)
 
-st.write("Files available in repository:")
-st.write(os.listdir("."))
+if st.button("Predict"):
+    data = np.array([
+        income,
+        co_income,
+        age,
+        credit_score,
+        loan_amount,
+        loan_term
+    ]).reshape(1, -1)
 
-st.success("Debug app loaded successfully!")
+    prediction = model.predict(data)
+
+    st.write(prediction)
